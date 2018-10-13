d553b002394ab99df089b1958d267684b6f793d0
Carmen Maria <carsoto8691@gmail.com>
2018-10-13 01:11:29 -0500

    Creación del middleware para autenticación por roles

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@ Route::group(['middleware' => 'auth'], function () {
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
 });
+/*
+	Route::put('post/{id}', function ($id) {
+	    //
+	})->middleware('auth', 'role:admin');
+*/
