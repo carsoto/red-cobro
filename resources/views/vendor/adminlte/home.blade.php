74698a85b280766371ff6c08c30281dedf34d708
Carmen Maria <carsoto8691@gmail.com>
2018-11-08 17:42:31 -0500

    Fix de la vista de inicio

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -4,17 +4,22 @@
 	{{ trans('adminlte_lang::message.home') }}
 @endsection
 
+@section('contentheader_title')
+	Dashboard
+@endsection
+
+@section('contentheader_description')
+@endsection
 
 @section('main-content')
 	<div class="container-fluid spark-screen">
 		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">Home</div>
-
-					<div class="panel-body">
+			<div class="col-lg-12">
+				<div class="box box-primary">
+					<!-- /.box-header -->
+					<div class="box-body">
 						@if(Auth::user()->hasRole('admin'))
-						    <div>Acceso como administrador</div>
+						    <div>Acceso administrador</div>
 
 						@elseif(Auth::user()->hasRole('supervisor'))
 						    <div>Acceso supervisor</div>
