abd4c011cba35abf9bc787955a1f13752c99d07e
Carmen Maria <carsoto8691@gmail.com>
2018-12-30 23:43:43 -0500

    Fix para mostrar RUT con dv en la tabla deudores

--- resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
@@ -88,7 +88,7 @@
 		        serverSide: true,
 		        ajax: 'deudores/table/listado',
 		        columns: [		
-		            {data: 'rut', name: 'rut'},
+		            {data: 'rut_dv', name: 'rut_dv'},
 		            {data: 'razon_social', name: 'razon_social'},
 		            {data: 'action', name: 'action', orderable: false}
 		        ]
